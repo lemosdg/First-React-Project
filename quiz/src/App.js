870d5a799d1dac86c9583be2274aa36cb6214e55
Diego Lemos <diegolemosgarcia@gmail.com>
2023-06-26 21:52:48 +0200

    array de logos creado

--- quiz/src/App.js
+++ quiz/src/App.js
@@ -4,11 +4,6 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
   return (
-    <article>
-      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8X_5ZQeRpoUjSzx7zxD90DlfpOMgtVZZldc2GgIOFMU9wGAbSR8ui&usqp=CAE&s' alt='lugo'/>
-      <section>
-        <TwitterFollowCard></TwitterFollowCard>
-      </section>
-    </article>
+    <TwitterFollowCard></TwitterFollowCard>
   )
 }
--- quiz/src/TwitterFollowCard.js
+++ quiz/src/TwitterFollowCard.js
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 
 export function TwitterFollowCard() {  
+  const equipo = [
+    {nombre: 'lugo', logo:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8X_5ZQeRpoUjSzx7zxD90DlfpOMgtVZZldc2GgIOFMU9wGAbSR8ui&usqp=CAE&s'},
+    {nombre: 'celta', logo:'https://upload.wikimedia.org/wikipedia/en/thumb/1/12/RC_Celta_de_Vigo_logo.svg/800px-RC_Celta_de_Vigo_logo.svg.png'}
+  ];
+
   const [inputValue, setInputValue] = useState('');
   const [showValue, setShowValue] = useState(false);
 
@@ -10,16 +15,18 @@ export function TwitterFollowCard() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setShowValue(inputValue === 'lugo'); // Realiza la comparación aquí
+    setShowValue(inputValue === equipo[0].nombre); 
   };
 
-
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={inputValue} onChange={handleChange} />
-      </form>
-      {showValue && <p>El valor coincide</p>}
-    </div>
+    <article>
+      <img src={equipo[0].logo} alt='equipo'/>
+      <section>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={inputValue} onChange={handleChange} />
+        </form>
+        {showValue && <p>El valor coincide</p>}
+      </section>
+    </article>
   )
 }
